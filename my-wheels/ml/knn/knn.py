c644ef918b39ba5baf975447341bd84e67687341
Shunliz <zsl6658@126.com>
2017-08-02 22:15:54 +0800

    update knn

--- my-wheels/ml/knn/knn.py
+++ my-wheels/ml/knn/knn.py
@@ -31,3 +31,19 @@ def classify0(inX,dataSet,labels,k):
     sortedClassCount = sorted(classCount.iteritems(),key = operator.itemgetter(1),reverse = True)
     #返回距离最小的点对应的标签
    return sortedClassCount[0][0]
+
+'''
+import kNN  
+from numpy import *   
+  
+dataSet, labels = kNN.createDataSet()  
+  
+testX = array([1.2, 1.0])  
+k = 3  
+outputLabel = kNN.kNNClassify(testX, dataSet, labels, 3)  
+print "Your input is:", testX, "and classified to class: ", outputLabel  
+  
+testX = array([0.1, 0.3])  
+outputLabel = kNN.kNNClassify(testX, dataSet, labels, 3)  
+print "Your input is:", testX, "and classified to class: ", outputLabel  
+'''
