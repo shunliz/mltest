f3de558a5513822e466ef59b18289d208d07c70b
ShunliZhou <zsl6658@126.com>
2019-12-22 18:06:24 +0800

    add pca

--- my-wheels/ml/pca.py
+++ my-wheels/ml/pca.py
@@ -1,22 +1,65 @@
-from numpy import *
+def pca(data, n_dim):
+    '''
+    pca is O(D^3)
+    :param data: (n_samples, n_features(D))
+    :param n_dim: target dimensions
+    :return: (n_samples, n_dim)
+    '''
+    data = data - np.mean(data, axis = 0, keepdims = True)
 
-def pca(x):
-	"""Performs principal component on x, a matrix with observations in the rows.
-	Returns the projection matrix (the eigenvectors of x^T x, ordered with largest eigenvectors first) and the eigenvalues (ordered from largest to smallest).
-	"""
-	
-	x = (x - x.mean(axis = 0)) # Subtract the mean of column i from column i, in order to center the matrix.
-	
-	num_observations, num_dimensions = x.shape
-	
-	# Often, we have a large number of dimensions (say, 10,000) but a relatively small number of observations (say, 75). In this case, instead of directly computing the eigenvectors of x^T x (a 10,000 x 10,000 matrix), it's more efficient to compute the eigenvectors of x x^T and translate these into the eigenvectors of x^T x by using the transpose trick. 
-	# The transpose trick says that if v is an eigenvector of M^T M, then Mv is an eigenvector of MM^T.
-	# We arbitrarily select "100" as the switching threshold. Another approach is to switch by comparing num_observations and num_dimensions.
-	if num_dimensions > 100:
-		eigenvalues, eigenvectors = linalg.eigh(dot(x, x.T))
-		v = (dot(x.T, eigenvectors).T)[::-1] # Unscaled, but the relative order is still correct.
-		s = sqrt(eigenvalues)[::-1] # Unscaled, but the relative order is still correct.
-	else:
-		u, s, v = linalg.svd(x, full_matrices = False)
-		
-	return v, s
+    cov = np.dot(data.T, data)
+
+    eig_values, eig_vector = np.linalg.eig(cov)
+    # print(eig_values)
+    indexs_ = np.argsort(-eig_values)[:n_dim]
+    picked_eig_values = eig_values[indexs_]
+    picked_eig_vector = eig_vector[:, indexs_]
+    data_ndim = np.dot(data, picked_eig_vector)
+    return data_ndim
+
+
+# data 降维的矩阵(n_samples, n_features)
+# n_dim 目标维度
+# fit n_features >> n_samples, reduce cal
+def highdim_pca(data, n_dim):
+    '''
+    when n_features(D) >> n_samples(N), highdim_pca is O(N^3)
+    :param data: (n_samples, n_features)
+    :param n_dim: target dimensions
+    :return: (n_samples, n_dim)
+    '''
+    N = data.shape[0]
+    data = data - np.mean(data, axis = 0, keepdims = True)
+
+    Ncov = np.dot(data, data.T)
+
+    Neig_values, Neig_vector = np.linalg.eig(Ncov)
+    indexs_ = np.argsort(-Neig_values)[:n_dim]
+    Npicked_eig_values = Neig_values[indexs_]
+    # print(Npicked_eig_values)
+    Npicked_eig_vector = Neig_vector[:, indexs_]
+    # print(Npicked_eig_vector.shape)
+
+    picked_eig_vector = np.dot(data.T, Npicked_eig_vector)
+    picked_eig_vector = picked_eig_vector/(N*Npicked_eig_values.reshape(-1, n_dim))**0.5
+    # print(picked_eig_vector.shape)
+
+    data_ndim = np.dot(data, picked_eig_vector)
+    return data_ndim
+
+if __name__ == "__main__":
+    data = load_iris()
+    X = data.data
+    Y = data.target
+    data_2d1 = pca(X, 2)
+    plt.figure(figsize=(8,4))
+    plt.subplot(121)
+    plt.title("my_PCA")
+    plt.scatter(data_2d1[:, 0], data_2d1[:, 1], c = Y)
+
+    sklearn_pca = PCA(n_components=2)
+    data_2d2 = sklearn_pca.fit_transform(X)
+    plt.subplot(122)
+    plt.title("sklearn_PCA")
+    plt.scatter(data_2d2[:, 0], data_2d2[:, 1], c = Y)
+    plt.show()
